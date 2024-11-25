5034d1fe316038b56c5783af7bd9df0c75a834b2
Dahn John <32991740+dahnjohn@users.noreply.github.com>
2024-11-25 09:13:01 +0800

    changed port to 4000

--- backend/server.ts
+++ backend/server.ts
@@ -9,7 +9,7 @@ import routes from "./routes";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 // Connect to MongoDB
 connectDB();
